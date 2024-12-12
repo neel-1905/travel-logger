f80de48fc531cf5a79b3ac96d1ce2f2cc4a0098b
neel-narwadkar-gh <neel@growhut.in>
2024-12-12 20:32:11 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <Button>
+          {/* <Loader2 /> */}
+          Click Me!
+        </Button>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
